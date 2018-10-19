cd371b4fd20a853dca640c6c5e20feaf8cbf1646
jimer <jimer@DESKTOP-B0HF2Q8.otterbein.edu>
2018-10-19 14:47:43 -0400

    tried many things, but nothing changed. commiting so I dont have to merge

--- pickapath/Prototype.java
+++ pickapath/Prototype.java
@@ -3,6 +3,7 @@ import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
