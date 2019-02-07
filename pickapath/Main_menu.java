919e60a2296a421afcb7ecf3504430fb854ee760
logan <logan@DESKTOP-M1AF12A>
2019-02-07 09:57:41 -0500

    added a comment to basic stroke to find line thickness

--- pickapath/Main_menu.java
+++ pickapath/Main_menu.java
@@ -1,9 +1,15 @@
 package pickapath;
 
+import java.awt.Dimension;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import java.io.File;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
 public class Main_menu {
 	public static void main(String[] args) {
@@ -15,10 +21,19 @@ public class Main_menu {
 		panel.add(player_mode);
 		frame.add(panel);
 		frame.setSize(400, 100);
+		frame.setMinimumSize(new Dimension(400,100));
 		frame.setDefaultCloseOperation(
 				JFrame.DISPOSE_ON_CLOSE);
 		frame.setVisible(true);
 		
+		
 	}
+	public class Button extends JPanel {
 
-}
+	}
+	
+
+	}
+	
+
+
