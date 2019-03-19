cdba908e67c758f2b2cabeb9eeed0d3a9062427a
Lucia Ristea <luciamristea@yahoo.com>
2019-03-19 10:02:06 -0400

    pushing again bc I didn't save

--- pickapath/Main.java
+++ pickapath/Main.java
@@ -200,15 +200,6 @@ public class Main extends JFrame {
 		JScrollBar verticalScroll = new JScrollBar(JScrollBar.VERTICAL);
 		horizontalScroll.setVisible(true);
 		verticalScroll.setVisible(true);
-		//Table stuff
-		/*
-		ItemTableModel tableModel = new ItemTableModel();
-		JTable itemTable = new JTable(tableModel);
-		itemTable.setFillsViewportHeight(true);
-		JScrollPane tableScroll = new JScrollPane(itemTable);
-		tableScroll.setPreferredSize(new Dimension(200, 500));
-		
-		panel.add(tableScroll, BorderLayout.WEST); */
 		
 		
 		panel.add(canvas, BorderLayout.CENTER);
