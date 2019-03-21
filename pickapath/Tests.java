eab6fa461647a0f371d13993055358e7cc02aedf
Lucia Ristea <luciamristea@yahoo.com>
2019-03-21 16:32:16 -0400

    improved another test

--- pickapath/Tests.java
+++ pickapath/Tests.java
@@ -22,12 +22,23 @@ class Tests {
 			
 	@Test
 	public void deleteAllBoxesTest() {
-		List<Arrow> arrows = new ArrayList<Arrow>();
-		List<Box> boxes = new ArrayList<Box>();
-		boxes.add(new Box(40,60,100,50, "Olivia"));
-		boxes.add(new Box(25,70,100,50, "Lucia"));
-		arrows.add(new Arrow (boxes.get(0), boxes.get(1), "friends"));
-		Canvas canvas = new Canvas(arrows, boxes, null);
+		
+		Main main = new Main();
+		Canvas canvas = main.getCanvas();
+		
+		Box box1 = new Box(40,60,100,50, "Olivia");
+		canvas.addBox(box1);
+		Box box2 = new Box(25,70,100,50, "Lucia");
+		canvas.addBox(box2);
+		
+		List<Arrow> arrows = canvas.getArrows();
+		arrows.add(new Arrow (box1, box2, "friends"));
+		
+		List<Box> boxes = canvas.getBoxes();
+		
+		canvas.mousePressed(new MouseEvent(canvas, MouseEvent.MOUSE_PRESSED, System.nanoTime(), 0, 30, 75, 1, false));
+		canvas.deleteBox();
+		main.dispose();
 		canvas.deleteAllBoxes();
 		Assert.assertEquals("boxes not deleted", true, boxes.size()==0 && arrows.size()==0);
 	}
